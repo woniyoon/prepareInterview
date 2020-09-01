0def84873671798884b12e9dac140eff4c10fcc6
woniyoon <gellifisch@gmail.com>
2020-09-01 22:33:13 +0900

    Solve findMrKim in js

--- practice_js.js
+++ practice_js.js
@@ -11,3 +11,23 @@ function watermelon(n) {
     }
     return answer;
 }
+
+// 서울에서 김서방 찾기
+// 내가 푼 방식
+function findMrKim(seoul) {
+    var answer = '';
+        
+    seoul.forEach((item, index) => {
+        if(item == "Kim") {
+            answer = `김서방은 ${index}에 있다`;
+        }   
+        break;
+    });
+    
+    return answer;
+}
+
+// 가장 간단한 방식
+function findMrKim2(seoul) {
+    return "김서방은 "+ seoul.indexOf("Kim") + "에 있다";
+}
