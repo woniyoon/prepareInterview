fda3521d95594f0af788a307a686642b841596db
woniyoon <gellifisch@gmail.com>
2020-09-07 20:55:49 +0900

    Solve findUncompleted2

--- hash_practice.js
+++ hash_practice.js
@@ -78,3 +78,35 @@ function findUncompleted(participant, completion) {
     return answer;
 }
 
+
+// 레벨 테스트하다가 또 나와서 다시 풀어본 완주하지 못한 선수
+// 이번에는 모던 자바스크립트 문서에 나온대로 Map 객체를 만들어서 해봤다!
+// 100점~!
+
+function findUnCompleted2(participant, completion) {
+    var answer = '';
+    let participated = new Map();
+    
+    for(var i = 0; i < participant.length; i++) {
+        let runner = participant[i];
+        if(participated.has(runner)) {
+            var value = participated.get(runner);
+            participated.set(runner, value+1);
+        } else {
+            participated.set(runner, 1);            
+        }
+    }
+    
+    for(let completed of completion) {
+        var value = participated.get(completed);
+        participated.set(completed, value+1);
+    }
+    
+    participated.forEach((value, key)=>{
+        if(value % 2 == 1) {
+            answer = key;
+        }
+    });
+    
+    return answer;
+}
